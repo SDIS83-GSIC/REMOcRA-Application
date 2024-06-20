7f3371a5da795e334c47ed35e820b801574898a5
Emilie Genton <emg@atolcd.com>
2024-06-20 09:35:45 +0200

    Ajoute le schéma pour la traçabilite...
    ... On l'ajoute dans la base mais aussi dans la génération de jooq
    
    Change-Id: Ie05304564cfacf3760119388c2579a94e85e0bb8

---
+++ db/src/main/resources/db/migration/V5__tracabilite.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS historique.tracabilite;
+DROP TYPE IF EXISTS historique.TYPE_OPERATION;
+DROP TYPE IF EXISTS historique.TYPE_OBJET;
+
+CREATE SCHEMA IF NOT EXISTS  historique;
+
+CREATE TYPE historique.TYPE_OPERATION AS ENUM ('INSERT', 'UPDATE', 'DELETE');
+
+CREATE TYPE historique.TYPE_OBJET AS ENUM (
+    'PEI',
+    'VISITE',
+    'PARAMETRE',
+    'GESTIONNAIRE',
+    'SITE'
+    -- D'autres idées ?
+);
+
+INSERT INTO remocra.utilisateur(utilisateur_id, utilisateur_actif, utilisateur_email, utilisateur_username, utilisateur_nom, utilisateur_prenom)
+VALUES (
+    gen_random_uuid(),
+    true,
+    'null',
+    'UTILISATEUR_SYSTEME',
+    'UTILISATEUR_SYSTEME',
+    'UTILISATEUR_SYSTEME'
+);
+
+CREATE TABLE historique.tracabilite (
+    tracabilite_id                  UUID                        PRIMARY KEY,
+    tracabilite_type_operation      historique.TYPE_OPERATION   NOT NULL,
+    tracabilite_date                TIMESTAMPTZ                 NOT NULL,
+    tracabilite_objet_id            UUID                        NOT NULL,
+    tracabilite_type_objet          historique.TYPE_OBJET       NOT NULL,
+    tracabilite_objet_data          JSONB                       NOT NULL,
+    tracabilite_auteur_id           UUID                        NOT NULL,
+    tracabilite_auteur_data         JSONB                       NOT NULL
+);
