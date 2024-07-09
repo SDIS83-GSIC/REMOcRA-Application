a4519e176de83ecf070975e41b7f476436204aaa
Hugo Bole <hbo@atolcd.com>
2024-07-09 12:12:29 +0200

    Rajoute des facilitateurs dans le ParametresProvider
    Change-Id: Ibeaf376fb5ce5032071a4a90fe9f63e9827badb7

--- app/src/main/kotlin/remocra/app/ParametresProvider.kt
+++ app/src/main/kotlin/remocra/app/ParametresProvider.kt
@@ -6,6 +6,8 @@ import jakarta.inject.Singleton
 import remocra.data.ParametresData
 import remocra.db.ParametreRepository
 import remocra.db.TaskRepository
+import remocra.db.jooq.remocra.enums.TypeParametre
+import remocra.db.jooq.remocra.tables.pojos.Parametre
 import remocra.schedule.SchedulableTasksExecutor
 
 @Singleton
@@ -38,5 +40,67 @@ constructor(
         val mapTasks = taskRepository.getMapTasks()
 
         return ParametresData(mapParametres = mapParametres, mapTasksInfo = mapTasks)
+    }
+
+    private fun getParametre(key: String): Parametre {
+        val value = get().mapParametres[key] ?: throw IllegalArgumentException("La clé $key n'existe pas dans les paramètres")
+
+        return value
+    }
+
+    fun getParametreValue(key: String): Any? {
+        return getParametre(key).parametreValeur
+    }
+
+    /**
+     * Retourne la valeur booléenne d'un paramètre dont la clé est fournie en paramètre.
+     *
+     * */
+    fun getParametreBoolean(key: String): Boolean? {
+        val param = getParametre(key)
+        if (TypeParametre.BOOLEAN != param.parametreType) {
+            throw IllegalArgumentException("Mauvais type demandé pour la clé $key")
+        }
+
+        return param.parametreValeur?.toBooleanStrictOrNull()
+    }
+
+    /**
+     * Retourne la valeur Int d'un paramètre dont la clé est fournie en paramètre.
+     *
+     * */
+    fun getParametreInt(key: String): Int? {
+        val param = getParametre(key)
+        if (TypeParametre.INTEGER != param.parametreType) {
+            throw IllegalArgumentException("Mauvais type demandé pour la clé $key")
+        }
+
+        return param.parametreValeur?.toIntOrNull()
+    }
+
+    /**
+     * Retourne la valeur Double d'un paramètre dont la clé est fournie en paramètre.
+     *
+     * */
+    fun getParametreDouble(key: String): Double? {
+        val param = getParametre(key)
+        if (TypeParametre.DOUBLE != param.parametreType) {
+            throw IllegalArgumentException("Mauvais type demandé pour la clé $key")
+        }
+
+        return param.parametreValeur?.toDoubleOrNull()
+    }
+
+    /**
+     * Retourne la valeur String d'un paramètre dont la clé est fournie en paramètre.
+     *
+     * */
+    fun getParametreString(key: String): String? {
+        val param = getParametre(key)
+        if (TypeParametre.STRING != param.parametreType) {
+            throw IllegalArgumentException("Mauvais type demandé pour la clé $key")
+        }
+
+        return param.parametreValeur
     }
 }
--- app/src/main/kotlin/remocra/data/ParametresData.kt
+++ app/src/main/kotlin/remocra/data/ParametresData.kt
@@ -1,9 +1,10 @@
 package remocra.data
 
 import remocra.db.jooq.remocra.enums.TypeTask
+import remocra.db.jooq.remocra.tables.pojos.Parametre
 import remocra.db.jooq.remocra.tables.pojos.Task
 
 data class ParametresData(
-    val mapParametres: Map<String, Any>,
+    val mapParametres: Map<String, Parametre>,
     val mapTasksInfo: Map<TypeTask, Task>,
 )
