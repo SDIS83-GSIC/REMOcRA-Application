2a58bd3559525ffb53b41fdef8b2c9d75e7039a4
Emilie Genton <emg@atolcd.com>
2024-07-12 09:29:13 +0200

    Met en place un accordion avec des states...
    ... Permet de gérer toutes les ouvertures / fermetures de sections.
    
    Change-Id: I313a79c8eae1c1d7049c8cd1e3e05188ae15338c

--- frontend/src/components/Accordion/Accordion.tsx
+++ frontend/src/components/Accordion/Accordion.tsx
@@ -1,12 +1,29 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { Accordion } from "react-bootstrap";
 
-const AccordionCustom = ({ list }: AccordionType) => {
+/**
+ * Composant Accordion qui permet d'avoir plusieurs sections
+ *
+ * @param {list} list - liste des sections avec leur nom et leur contenu
+ * @param {Function} handleShowClose - Fonction qui permet de fermer / ouvrir une section (elle doit venir de useAccordionState)
+ * @param {list} activesKeys - Liste des sections qui doivent être actives
+ */
+const AccordionCustom = ({
+  list,
+  handleShowClose,
+  activesKeys,
+}: AccordionType) => {
   return (
-    <Accordion alwaysOpen defaultActiveKey="0">
+    <Accordion
+      alwaysOpen
+      defaultActiveKey={activesKeys}
+      activeKey={activesKeys}
+    >
       {list.map(({ header, content }, key) => (
         <Accordion.Item eventKey={key.toString()} key={key}>
-          <Accordion.Header>{header}</Accordion.Header>
+          <Accordion.Header onClick={() => handleShowClose(key)}>
+            {header}
+          </Accordion.Header>
           <Accordion.Body>{content}</Accordion.Body>
         </Accordion.Item>
       ))}
@@ -18,4 +35,41 @@ export default AccordionCustom;
 
 type AccordionType = {
   list: { header: string; content: ReactNode }[];
+  handleShowClose: (index: number) => void;
+  activesKeys: string[];
 };
+
+/**
+ * Permet d'observer le state d'un accordion
+ * @param initialeOpenState une liste de boolean : un booléen par section (true = ouvert, false = fermé)
+ * @returns l'état courant de openState, la fonction handleShowClose qui permet de fermer / ouvrir une section et les indexes des sections ouvertes
+ */
+export function useAccordionState(initialeOpenState = [true]) {
+  const [openState, setOpenState] = useState(initialeOpenState);
+  const [activesKeys, setActivesKeys] = useState(
+    initialeOpenState
+      .map((e, i) => (e === true ? i.toString() : ""))
+      .filter(String),
+  );
+
+  function handleShowClose(index: number) {
+    openState[index] = !openState[index];
+    setOpenState(openState);
+    getActivesKeys();
+  }
+
+  function getActivesKeys() {
+    setActivesKeys(
+      openState.map((e, i) => (e === true ? i.toString() : "")).filter(String),
+    );
+  }
+
+  // Force l'ouverture
+  function show(index: number) {
+    openState[index] = true;
+    setOpenState(openState);
+    getActivesKeys();
+  }
+
+  return { openState, handleShowClose, activesKeys, show };
+}
