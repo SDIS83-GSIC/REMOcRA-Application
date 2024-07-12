46f3238c60b385ee596e0f7dfce99876b853e578
Clément GRENOT <cgr@atolcd.com>
2024-07-12 12:22:25 +0200

    Ajoute un UseCase remontant l'intégralité des infos des visites d'un pei
    Change-Id: I6492092ea70c2d0555f039572aef8d0b078ace7e

--- app/src/main/kotlin/remocra/db/VisiteRepository.kt
+++ app/src/main/kotlin/remocra/db/VisiteRepository.kt
@@ -2,11 +2,17 @@ package remocra.db
 
 import com.google.inject.Inject
 import org.jooq.DSLContext
+import remocra.db.jooq.remocra.enums.TypeVisite
 import remocra.db.jooq.remocra.tables.pojos.Visite
 import remocra.db.jooq.remocra.tables.pojos.VisiteCtrlDebitPression
+import remocra.db.jooq.remocra.tables.references.ANOMALIE
+import remocra.db.jooq.remocra.tables.references.ANOMALIE_CATEGORIE
 import remocra.db.jooq.remocra.tables.references.L_VISITE_ANOMALIE
+import remocra.db.jooq.remocra.tables.references.PEI
+import remocra.db.jooq.remocra.tables.references.POIDS_ANOMALIE
 import remocra.db.jooq.remocra.tables.references.VISITE
 import remocra.db.jooq.remocra.tables.references.VISITE_CTRL_DEBIT_PRESSION
+import java.time.ZonedDateTime
 import java.util.UUID
 
 class VisiteRepository
@@ -31,4 +37,74 @@ class VisiteRepository
         .where(VISITE.PEI_ID.eq(peiId))
         .orderBy(VISITE.DATE.desc())
         .fetchOneInto()
+
+    fun getAllVisiteByPeiId(peiId: UUID): List<VisiteComplete> =
+        dsl.select(VISITE.fields().toList())
+            .from(VISITE)
+            .where(VISITE.PEI_ID.eq(peiId))
+            .orderBy(VISITE.DATE.desc())
+            .fetch()
+            .map { record ->
+                VisiteComplete(
+                    visiteId = record.getValue(VISITE.ID)!!,
+                    visitePeiId = record.getValue(VISITE.PEI_ID)!!,
+                    visiteDate = record.getValue(VISITE.DATE)!!,
+                    visiteTypeVisite = record.getValue(VISITE.TYPE_VISITE)!!,
+                    visiteAgent1 = record.getValue(VISITE.AGENT1),
+                    visiteAgent2 = record.getValue(VISITE.AGENT2),
+                    visiteObservation = record.getValue(VISITE.OBSERVATION),
+                    listeAnomalie = null,
+                    ctrlDebitPression = null,
+                )
+            }
+
+    data class VisiteComplete(
+        val visiteId: UUID,
+        val visitePeiId: UUID,
+        val visiteDate: ZonedDateTime,
+        val visiteTypeVisite: TypeVisite,
+        val visiteAgent1: String?,
+        val visiteAgent2: String?,
+        val visiteObservation: String?,
+        var listeAnomalie: MutableList<CompletedAnomalie>?,
+        var ctrlDebitPression: VisiteCtrlDebitPression?,
+    )
+
+    fun getCompletedAnomalieByPeiId(peiUUID: UUID): MutableMap<UUID?, MutableList<CompletedAnomalie>?> =
+        dsl.select(
+            ANOMALIE.ID,
+            ANOMALIE.CODE,
+            ANOMALIE.LIBELLE,
+            ANOMALIE.COMMENTAIRE,
+            ANOMALIE_CATEGORIE.ID,
+            ANOMALIE_CATEGORIE.LIBELLE,
+            POIDS_ANOMALIE.VAL_INDISPO_TERRESTRE,
+            POIDS_ANOMALIE.VAL_INDISPO_HBE,
+            L_VISITE_ANOMALIE.VISITE_ID,
+        )
+            .from(ANOMALIE)
+            .join(ANOMALIE_CATEGORIE).on(ANOMALIE_CATEGORIE.ID.eq(ANOMALIE.ANOMALIE_CATEGORIE_ID))
+            .join(L_VISITE_ANOMALIE).on(L_VISITE_ANOMALIE.ANOMALIE_ID.eq(ANOMALIE.ID))
+            .join(VISITE).on(VISITE.ID.eq(L_VISITE_ANOMALIE.VISITE_ID))
+            .join(PEI).on(PEI.ID.eq(peiUUID))
+            .join(POIDS_ANOMALIE)
+            .on(POIDS_ANOMALIE.ANOMALIE_ID.eq(ANOMALIE.ID))
+            .and(POIDS_ANOMALIE.NATURE_ID.eq(PEI.NATURE_ID))
+            .fetchGroups(L_VISITE_ANOMALIE.VISITE_ID, CompletedAnomalie::class.java)
+
+    data class CompletedAnomalie(
+        val anomalieId: UUID,
+        val anomalieCode: String,
+        val anomalieLibelle: String,
+        val anomalieCommentaire: String?,
+        val anomalieCategorieId: UUID,
+        val anomalieCategorieLibelle: String,
+        val poidsAnomalieValIndispoTerrestre: Int?,
+        val poidsAnomalieValIndispoHbe: Int?,
+    )
+
+    fun getAllCtrlByListVisiteId(listVisiteId: List<UUID>): List<VisiteCtrlDebitPression> =
+        dsl.selectFrom(VISITE_CTRL_DEBIT_PRESSION)
+            .where(VISITE_CTRL_DEBIT_PRESSION.VISITE_ID.`in`(listVisiteId))
+            .fetchInto()
 }
---
+++ app/src/main/kotlin/remocra/usecases/visites/GetVisiteWithAnomalies.kt
@@ -0,0 +1,22 @@
+package remocra.usecases.visites
+
+import com.google.inject.Inject
+import remocra.db.VisiteRepository
+import java.util.UUID
+
+class GetVisiteWithAnomalies {
+
+    @Inject lateinit var visiteRepository: VisiteRepository
+
+    fun getVisiteWithAnomalies(peiUUID: UUID): List<VisiteRepository.VisiteComplete> {
+        val listVisite = visiteRepository.getAllVisiteByPeiId(peiUUID)
+        val listCtrl = visiteRepository.getAllCtrlByListVisiteId(listVisite.map { it.visiteId })
+        val listAnomalieOrderedByVisite = visiteRepository.getCompletedAnomalieByPeiId(peiUUID)
+
+        listVisite.forEach { visite ->
+            visite.ctrlDebitPression = listCtrl.firstOrNull { it.visiteCtrlDebitPressionVisiteId == visite.visiteId }
+            visite.listeAnomalie = listAnomalieOrderedByVisite[visite.visiteId]
+        }
+        return listVisite
+    }
+}
