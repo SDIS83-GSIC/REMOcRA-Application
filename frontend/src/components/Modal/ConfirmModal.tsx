d813a6b3dc40b4b45c7ea85c1926127a931ffc7e
Renaud KIEFFER <rki@atolcd.com>
2024-10-16 15:45:46 +0200

    Modifie la ConfirmModal
    ... Ajoute la ref
    permet de l'utiliser en PUT
    
    Change-Id: I81b2922918b7b91f3ea8a666ae8541cd5050a042

--- frontend/src/components/Modal/ConfirmModal.tsx
+++ frontend/src/components/Modal/ConfirmModal.tsx
@@ -1,20 +1,27 @@
 import Modal from "react-bootstrap/Modal";
 import { ReactNode } from "react";
 import { Button } from "react-bootstrap";
-import { usePost } from "../Fetch/useFetch.tsx";
+import { usePost, usePut } from "../Fetch/useFetch.tsx";
 import ToastAutohide from "../../module/Toast/ToastAutoHide.tsx";
 import { useToastContext } from "../../module/Toast/ToastProvider.tsx";
 
-const ConfirmModalBody = ({
-  query,
+type FunctionType = {
+  id;
+  query;
+  onConfirm;
+  successToast;
+  closeModal;
+  errorToast;
+};
+const usePostState = ({
   id,
-  closeModal,
+  query,
   onConfirm,
-  content,
-  href,
-}: ConfirmModalBodyType) => {
-  const { success: successToast, error: errorToast } = useToastContext();
-  const action = usePost(id ? `${query}/${id}` : `${query}`, {
+  successToast,
+  closeModal,
+  errorToast,
+}: FunctionType) => {
+  return usePost(id ? `${query}/${id}` : `${query}`, {
     onResolve: (res: any) => {
       onConfirm && onConfirm(res);
       successToast({ message: "L'action a bien été exécutée" });
@@ -26,6 +33,62 @@ const ConfirmModalBody = ({
       });
       closeModal();
     },
+  });
+};
+
+const usePutState = ({
+  id,
+  query,
+  onConfirm,
+  successToast,
+  closeModal,
+  errorToast,
+}) => {
+  return usePut(
+    id ? `${query}/${id}` : `${query}`,
+    {
+      onResolve: (res: any) => {
+        onConfirm && onConfirm(res);
+        successToast({ message: "L'action a bien été exécutée" });
+        closeModal();
+      },
+      onReject: async (error: any) => {
+        errorToast({
+          message: `Erreur lors de l'exécution de l'action : ${await error.text()}`,
+        });
+        closeModal();
+      },
+    },
+    false,
+  );
+};
+
+const ConfirmModalBody = ({
+  query,
+  id,
+  closeModal,
+  onConfirm,
+  content,
+  href,
+  isPost,
+}: ConfirmModalBodyType) => {
+  const { success: successToast, error: errorToast } = useToastContext();
+  const usePost = usePostState({
+    id: id,
+    query: query,
+    closeModal: closeModal,
+    onConfirm: onConfirm,
+    errorToast: errorToast,
+    successToast: successToast,
+  });
+
+  const usePut = usePutState({
+    id: id,
+    query: query,
+    closeModal: closeModal,
+    onConfirm: onConfirm,
+    errorToast: errorToast,
+    successToast: successToast,
   });
 
   return (
@@ -41,7 +104,7 @@ const ConfirmModalBody = ({
             // Si on a un href, on n'est pas en post
             if (href === undefined) {
               try {
-                await action.run();
+                isPost ? await usePost.run() : await usePut.run();
               } catch (e: any) {
                 ToastAutohide({
                   content: `Erreur lors de l'exécution de l'action : ${e.message}`,
@@ -62,6 +125,7 @@ const ConfirmModalBody = ({
 };
 
 const ConfirmModal = ({
+  ref,
   visible,
   closeModal,
   header = "Confirmation",
@@ -70,9 +134,10 @@ const ConfirmModal = ({
   onConfirm,
   content = "Confirmer ?",
   href = undefined,
+  isPost = true,
 }: ConfirmModalBodyType & { visible: boolean }) => {
   return (
-    <Modal show={visible} onHide={closeModal}>
+    <Modal show={visible} onHide={closeModal} ref={ref}>
       <Modal.Header>
         <Modal.Title>{header}</Modal.Title>
       </Modal.Header>
@@ -83,6 +148,7 @@ const ConfirmModal = ({
         onConfirm={onConfirm}
         content={content}
         href={href}
+        isPost={isPost}
       />
     </Modal>
   );
@@ -95,8 +161,10 @@ type ConfirmModalBodyType = {
   id?: string;
   query: string;
   onConfirm?: (value: any) => void;
-  content: ReactNode;
+  content?: ReactNode;
   href?: string | undefined;
+  ref?: any;
+  isPost?: boolean;
 };
 
 export default ConfirmModal;
