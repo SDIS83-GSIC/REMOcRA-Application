abeb7cb779bb7053c72fb5d58cec9051c94b70ad
Clément GRENOT <cgr@atolcd.com>
2024-10-31 12:06:19 +0100

    Passe les contact_role existant à protected
    DESTINATAIRE_MAIRE_ROP et CHANGEMENT_ETAT_PEI
    
    Change-Id: I9a3c149d3d208822bfc259d226d4ddb05c8ef3d8

---
+++ db/src/main/resources/db/migration/V43__role_contact_protected.sql
@@ -0,0 +1,7 @@
+UPDATE remocra.role_contact
+SET role_contact_protected=true
+WHERE role_contact_code='DESTINATAIRE_MAIRE_ROP';
+
+UPDATE remocra.role_contact
+SET role_contact_protected=true
+WHERE role_contact_code='CHANGEMENT_ETAT_PEI';
