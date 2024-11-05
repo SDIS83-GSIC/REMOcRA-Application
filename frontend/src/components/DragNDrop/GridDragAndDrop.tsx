bdc8ff7032d7c92f1898c2ac4b1f2ec7d3f2efdc
Emilie Genton <emg@atolcd.com>
2024-11-05 08:20:23 +0100

    Créer un composant avec le MoveGrid et le addRemove...
    ... permet d'avoir une partie formulaire à répéter et la partie emplacement
    
    Change-Id: Ifbfd45d8fe636b78f6bff442b06d128106e8ef92

---
+++ frontend/src/components/DragNDrop/GridDragAndDrop.tsx
@@ -0,0 +1,244 @@
+import { useFormikContext } from "formik";
+import { ReactNode, useEffect, useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { URLS } from "../../routes.tsx";
+import AddRemoveComponent from "../AddRemoveComponent/AddRemoveComponent.tsx";
+import PageTitle from "../Elements/PageTitle/PageTitle.tsx";
+import { FormContainer } from "../Form/Form.tsx";
+import MyFormik from "../Form/MyFormik.tsx";
+import SubmitFormButtons from "../Form/SubmitFormButtons.tsx";
+import {
+  IconDragNDrop,
+  IconNextPage,
+  IconPreviousPage,
+} from "../Icon/Icon.tsx";
+import MoveGridComponent from "./MoveGridComponent.tsx";
+
+const GridDragAndDrop = ({
+  data,
+  colonneProperty,
+  ligneProperty,
+  titreProperty,
+  pageTitle,
+  urlToSubmit,
+  validationSchema,
+  getInitialValues,
+  prepareVariables,
+  createComponentToRepeat,
+  name,
+  defaultElement,
+  disabledSuivant,
+  isMultipartFormData = false,
+}: {
+  data: any[];
+  colonneProperty: string;
+  ligneProperty: string;
+  titreProperty: string;
+  pageTitle: string;
+  urlToSubmit: string;
+  validationSchema: any;
+  getInitialValues: (e: any) => any;
+  prepareVariables: (e: any) => any;
+  createComponentToRepeat: (index: number, listeElements: any[]) => ReactNode;
+  name: string;
+  defaultElement: any;
+  disabledSuivant: (element: any) => boolean;
+  isMultipartFormData: boolean;
+}) => {
+  const [items, setItems] = useState<any[]>();
+
+  useEffect(() => {
+    if (data) {
+      setItems(
+        Object.entries(
+          Object.groupBy(data, (element) => element[colonneProperty]),
+        )?.map(([, value]) =>
+          Object.values(value).map((t) => t[titreProperty]),
+        ),
+      );
+    }
+  }, [data, setItems, titreProperty, colonneProperty, ligneProperty]);
+
+  return (
+    items && (
+      <Container>
+        <Row>
+          <PageTitle icon={<IconDragNDrop />} title={pageTitle} />
+        </Row>
+        <MyFormik
+          initialValues={getInitialValues(data)}
+          validationSchema={validationSchema}
+          isPost={false}
+          isMultipartFormData={isMultipartFormData}
+          submitUrl={urlToSubmit}
+          prepareVariables={(values) => prepareVariables(values)}
+          redirectUrl={URLS.MODULE_ADMIN}
+        >
+          <Form
+            items={items}
+            setItems={setItems}
+            colonneProperty={colonneProperty}
+            createComponentToRepeat={createComponentToRepeat}
+            name={name}
+            defaultElement={defaultElement}
+            ligneProperty={ligneProperty}
+            titreProperty={titreProperty}
+            disabledSuivant={disabledSuivant}
+          />
+        </MyFormik>
+      </Container>
+    )
+  );
+};
+
+export default GridDragAndDrop;
+
+const Form = ({
+  items,
+  setItems,
+  name,
+  defaultElement,
+  createComponentToRepeat,
+  colonneProperty,
+  ligneProperty,
+  titreProperty,
+  disabledSuivant,
+}: {
+  items: any;
+  setItems: (e: any) => void;
+  name: string;
+  defaultElement: any;
+  createComponentToRepeat: (index: number, listeElements: any[]) => ReactNode;
+  colonneProperty: string;
+  ligneProperty: string;
+  titreProperty: string;
+  disabledSuivant: (element: any) => boolean;
+}) => {
+  const { values, setFieldValue } = useFormikContext();
+  const [possibilites, setPossibilites] = useState();
+  const [show, setShow] = useState(false);
+
+  return (
+    <FormContainer>
+      {!show ? (
+        <>
+          <Row>
+            <h3>1 - Choisissez les blocs qui apparaîtront</h3>
+            <AddRemoveComponent
+              name={name}
+              createComponentToRepeat={createComponentToRepeat}
+              defaultElement={defaultElement}
+              listeElements={values[name]}
+            />
+          </Row>
+          <Row>
+            <Col className="my-3 d-flex justify-content-center">
+              <Button
+                onClick={() => {
+                  setShow(true);
+
+                  // Si une valeur a été supprimée, on l'enlève de la liste
+                  let p = values[name]
+                    .filter((e) => e[colonneProperty] == null)
+                    .map((e) => e[titreProperty]);
+                  let itemsTemp = {};
+                  Object.entries(items).map(([key, value]) => {
+                    if (key === "possibilites") {
+                      p = new Set(
+                        value
+                          .filter((e) =>
+                            values[name]
+                              .map((t) => t[titreProperty])
+                              .includes(e),
+                          )
+                          .concat(p),
+                      );
+                    } else {
+                      itemsTemp[key] = value.filter((e) =>
+                        values[name].map((t) => t[titreProperty]).includes(e),
+                      );
+                    }
+                  });
+
+                  // Si on a changé le titre d'un bloc, on remet à jour la liste
+                  if (
+                    Object.values(itemsTemp).flat().length !==
+                    values[name].length
+                  ) {
+                    itemsTemp = Object.entries(
+                      Object.groupBy(
+                        values[name].filter((e) => e[colonneProperty] != null),
+                        (element) => element[colonneProperty],
+                      ),
+                    )?.map(([, value]) =>
+                      Object.values(value).map((t) => t[titreProperty]),
+                    );
+                  }
+
+                  // On enlève les valeurs qui sont déjà placées
+                  p = [...p].filter(
+                    (e) => !Object.values(itemsTemp).flat().includes(e),
+                  );
+                  setPossibilites(p);
+                  setItems({
+                    ...itemsTemp,
+                    possibilites: p,
+                  });
+                }}
+                disabled={values[name].find((e) => disabledSuivant(e))}
+              >
+                Suivant <IconNextPage />
+              </Button>
+            </Col>
+          </Row>
+        </>
+      ) : (
+        <>
+          <Row>
+            <h3>2 - Choisissez l&apos;emplacement de ces blocs</h3>
+            <MoveGridComponent
+              items={{
+                ...items,
+                possibilites: possibilites,
+              }}
+              setPossibilites={setPossibilites}
+              setItems={setItems}
+            />
+          </Row>
+          <Row>
+            <Col className="my-3 d-flex justify-content-center">
+              <Button onClick={() => setShow(false)}>
+                <IconPreviousPage /> Précédent
+              </Button>
+            </Col>
+          </Row>
+          <SubmitFormButtons
+            returnLink={URLS.MODULE_ADMIN}
+            disabledValide={items.possibilites.length > 0}
+            onClick={() => {
+              const listeWithColRow = [];
+              // On récupère les colonnes et les lignes
+              let indexI = 0;
+              for (const i in items) {
+                indexI++;
+                let indexJ = 0;
+                for (const j in items[i]) {
+                  indexJ++;
+                  const bloc = values[name].find(
+                    (e) => e[titreProperty] === items[i][j],
+                  );
+
+                  bloc[colonneProperty] = indexI;
+                  bloc[ligneProperty] = indexJ;
+
+                  listeWithColRow.push(bloc);
+                }
+              }
+              setFieldValue(name, listeWithColRow);
+            }}
+          />
+        </>
+      )}
+    </FormContainer>
+  );
+};
--- frontend/src/pages/Admin/resume/AdminFicheResume.tsx
+++ frontend/src/pages/Admin/resume/AdminFicheResume.tsx
@@ -1,26 +1,11 @@
 import { useFormikContext } from "formik";
-import { useEffect, useState } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import { object } from "yup";
-import AddRemoveComponent from "../../../components/AddRemoveComponent/AddRemoveComponent.tsx";
-import PageTitle from "../../../components/Elements/PageTitle/PageTitle.tsx";
+import GridDragAndDrop from "../../../components/DragNDrop/GridDragAndDrop.tsx";
 import { useGet } from "../../../components/Fetch/useFetch.tsx";
-import {
-  FormContainer,
-  SelectInput,
-  TextInput,
-} from "../../../components/Form/Form.tsx";
-import MyFormik from "../../../components/Form/MyFormik.tsx";
-import SubmitFormButtons from "../../../components/Form/SubmitFormButtons.tsx";
-import {
-  IconDragNDrop,
-  IconNextPage,
-  IconPreviousPage,
-} from "../../../components/Icon/Icon.tsx";
+import { SelectInput, TextInput } from "../../../components/Form/Form.tsx";
 import TYPE_RESUME_ELEMENT from "../../../enums/TypeResumeElementEnum.tsx";
 import url from "../../../module/fetch.tsx";
-import { URLS } from "../../../routes.tsx";
-import MoveGridComponent from "../../../components/DragNDrop/MoveGridComponent.tsx";
 
 type FicheResumeElementType = {
   ficheResumeBlocTypeResumeData: TYPE_RESUME_ELEMENT;
@@ -40,196 +25,37 @@ export const prepareVariables = (values) => ({
 
 const AdminFicheResume = () => {
   const { data } = useGet(url`/api/fiche-resume/get-blocs`);
-  const [items, setItems] = useState();
-
-  useEffect(() => {
-    if (data) {
-      setItems(
-        Object.entries(
-          Object.groupBy(
-            data,
-            ({ ficheResumeBlocColonne }) => ficheResumeBlocColonne,
-          ),
-        )?.map(([, value]) =>
-          Object.values(value).map((t) => t.ficheResumeBlocTitre),
-        ),
-      );
-    }
-  }, [data, setItems]);
 
   return (
-    data &&
-    items && (
-      <Container>
-        <Row>
-          <PageTitle
-            icon={<IconDragNDrop />}
-            title={"Paramétrage de la fiche résumé"}
-          />
-        </Row>
-        <MyFormik
-          initialValues={getInitialValues(data)}
-          validationSchema={validationSchema}
-          isPost={false}
-          submitUrl={`/api/fiche-resume/upsert`}
-          prepareVariables={(values) => prepareVariables(values)}
-          redirectUrl={URLS.MODULE_ADMIN}
-        >
-          <FormFicheResume items={items} setItems={setItems} data={data} />
-        </MyFormik>
-      </Container>
+    data && (
+      <GridDragAndDrop
+        data={data}
+        colonneProperty={"ficheResumeBlocColonne"}
+        ligneProperty={"ficheResumeBlocLigne"}
+        titreProperty={"ficheResumeBlocTitre"}
+        pageTitle={"Paramétrer la fiche résumé"}
+        urlToSubmit={`/api/fiche-resume/upsert`}
+        validationSchema={validationSchema}
+        getInitialValues={getInitialValues}
+        prepareVariables={prepareVariables}
+        createComponentToRepeat={createComponentToRepeat}
+        name={"listeFicheResumeElement"}
+        defaultElement={{
+          ficheResumeBlocTypeResumeData: null,
+          ficheResumeBlocTitre: null,
+          ficheResumeBlocColonne: null,
+          ficheResumeBlocLigne: null,
+        }}
+        disabledSuivant={(e: FicheResumeElementType) =>
+          e.ficheResumeBlocTypeResumeData == null ||
+          e.ficheResumeBlocTitre == null
+        }
+      />
     )
   );
 };
 
 export default AdminFicheResume;
-
-const FormFicheResume = ({
-  items,
-  setItems,
-}: {
-  items: any;
-  setItems: (e: any) => void;
-}) => {
-  const { values, setFieldValue } = useFormikContext();
-  const [possibilites, setPossibilites] = useState();
-  const [show, setShow] = useState(false);
-
-  return (
-    <FormContainer>
-      {!show ? (
-        <>
-          <Row>
-            <h3>1 - Choisissez les blocs qui apparaîteront</h3>
-            <AddRemoveComponent
-              name="listeFicheResumeElement"
-              createComponentToRepeat={createComponentToRepeat}
-              defaultElement={{
-                ficheResumeBlocTypeResumeData: null,
-                ficheResumeBlocTitre: null,
-                ficheResumeBlocColonne: null,
-                ficheResumeBlocLigne: null,
-              }}
-              listeElements={values.listeFicheResumeElement}
-            />
-          </Row>
-          <Row>
-            <Col className="my-3 d-flex justify-content-center">
-              <Button
-                onClick={() => {
-                  setShow(true);
-                  // Si une valeur a été supprimée, on l'enlève de la liste
-                  let p = values.listeFicheResumeElement
-                    .filter((e) => e.ficheResumeBlocColonne == null)
-                    .map((e) => e.ficheResumeBlocTitre);
-                  let itemsTemp = {};
-                  Object.entries(items).map(([key, value]) => {
-                    if (key === "possibilites") {
-                      p = new Set(
-                        value
-                          .filter((e) =>
-                            values.listeFicheResumeElement
-                              .map((t) => t.ficheResumeBlocTitre)
-                              .includes(e),
-                          )
-                          .concat(p),
-                      );
-                    } else {
-                      itemsTemp[key] = value.filter((e) =>
-                        values.listeFicheResumeElement
-                          .map((t) => t.ficheResumeBlocTitre)
-                          .includes(e),
-                      );
-                    }
-                  });
-
-                  // Si on a changé le titre d'un bloc, on remet à jour la liste
-                  if (
-                    Object.values(itemsTemp).flat().length !==
-                    values.listeFicheResumeElement.length
-                  ) {
-                    itemsTemp = Object.entries(
-                      Object.groupBy(
-                        values.listeFicheResumeElement,
-                        ({ ficheResumeBlocColonne }) => ficheResumeBlocColonne,
-                      ),
-                    )?.map(([, value]) =>
-                      Object.values(value).map((t) => t.ficheResumeBlocTitre),
-                    );
-                  }
-
-                  // On enlève les valeurs qui sont déjà placées
-                  p = [...p].filter(
-                    (e) => !Object.values(itemsTemp).flat().includes(e),
-                  );
-                  setPossibilites(p);
-                  setItems({
-                    ...itemsTemp,
-                    possibilites: p,
-                  });
-                }}
-                disabled={values.listeFicheResumeElement.find(
-                  (e) =>
-                    e.ficheResumeBlocTitre == null ||
-                    e.ficheResumeBlocTypeResumeData == null,
-                )}
-              >
-                Suivant <IconNextPage />
-              </Button>
-            </Col>
-          </Row>
-        </>
-      ) : (
-        <>
-          <Row>
-            <h3>2 - Choisissez l&apos;emplacement de ces blocs</h3>
-            <MoveGridComponent
-              items={{
-                ...items,
-                possibilites: possibilites,
-              }}
-              setPossibilites={setPossibilites}
-              setItems={setItems}
-            />
-          </Row>
-          <Row>
-            <Col className="my-3 d-flex justify-content-center">
-              <Button onClick={() => setShow(false)}>
-                <IconPreviousPage /> Précédent
-              </Button>
-            </Col>
-          </Row>
-          <SubmitFormButtons
-            returnLink={URLS.MODULE_ADMIN}
-            disabledValide={items.possibilites.length > 0}
-            onClick={() => {
-              const listeWithColRow: FicheResumeElementType[] = [];
-              // On récupère les colonnes et les lignes
-              let indexI = 0;
-              for (const i in items) {
-                indexI++;
-                let indexJ = 0;
-                for (const j in items[i]) {
-                  indexJ++;
-                  const bloc: FicheResumeElementType =
-                    values.listeFicheResumeElement.find(
-                      (e) => e.ficheResumeBlocTitre === items[i][j],
-                    );
-
-                  bloc.ficheResumeBlocColonne = indexI;
-                  bloc.ficheResumeBlocLigne = indexJ;
-
-                  listeWithColRow.push(bloc);
-                }
-              }
-              setFieldValue("listeFicheResumeElement", listeWithColRow);
-            }}
-          />
-        </>
-      )}
-    </FormContainer>
-  );
-};
 
 const ComposantToRepeat = ({
   index,
@@ -269,7 +95,7 @@ const ComposantToRepeat = ({
               (type) =>
                 type.id === listeElements[index].ficheResumeBlocTypeResumeData,
             )}
-            required={false}
+            required={true}
           />
         </Col>
         <Col>
