007ef32830722bdaada8dde991b6813e27cbab53
Thomas Broyer <tbr@atolcd.com>
2025-01-08 10:54:08 +0100

    Le Gradle Plugin Portal redirige désormais vers Central
    Change-Id: I22352ece9cdbeaae638269288da6e48e5908e541

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,18 +1,5 @@
-pluginManagement {
-    repositories {
-        // gradlePluginPortal redirige vers JCenter qui n'est pas fiable,
-        // on préfère Central à la place de JCenter (pour les mêmes dépendances)
-        // cf. https://github.com/gradle/gradle/issues/15406
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
 dependencyResolutionManagement {
     repositories {
-        // gradlePluginPortal redirige vers JCenter qui n'est pas fiable,
-        // on préfère Central à la place de JCenter (pour les mêmes dépendances)
-        // cf. https://github.com/gradle/gradle/issues/15406
-        mavenCentral()
         gradlePluginPortal()
     }
     versionCatalogs {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,6 @@
 rootProject.name = "remocra"
 
 pluginManagement {
-    repositories {
-        // gradlePluginPortal redirige vers JCenter qui n'est pas fiable,
-        // on préfère Central à la place de JCenter (pour les mêmes dépendances)
-        // cf. https://github.com/gradle/gradle/issues/15406
-        mavenCentral()
-        gradlePluginPortal()
-    }
     includeBuild("build-logic")
 }
 plugins {
