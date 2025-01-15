47ad0c7509407b3c095cee4cf94d3d31334d7d6e
Emilie Genton <emg@atolcd.com>
2025-01-15 09:43:45 +0100

    Corrige le calcul d'indispo d'un PEI
    Change-Id: I58da06b42e1319a23baf9a086d9fa0fdfcf59b35

--- app/src/main/kotlin/remocra/db/PoidsAnomalieRepository.kt
+++ app/src/main/kotlin/remocra/db/PoidsAnomalieRepository.kt
@@ -3,17 +3,32 @@ package remocra.db
 import com.google.inject.Inject
 import org.jooq.DSLContext
 import org.jooq.impl.DSL
+import remocra.GlobalConstants
 import remocra.db.jooq.remocra.enums.TypeVisite
 import remocra.db.jooq.remocra.tables.pojos.PoidsAnomalie
+import remocra.db.jooq.remocra.tables.references.ANOMALIE
+import remocra.db.jooq.remocra.tables.references.ANOMALIE_CATEGORIE
 import remocra.db.jooq.remocra.tables.references.POIDS_ANOMALIE
 import java.util.UUID
 
 class PoidsAnomalieRepository @Inject constructor(private val dsl: DSLContext) : AbstractRepository() {
 
     fun getPoidsAnomalies(anomaliesIds: Collection<UUID>, natureId: UUID, typeVisite: TypeVisite?): Collection<PoidsAnomalie> =
-        dsl.selectFrom(POIDS_ANOMALIE)
+        dsl.select(POIDS_ANOMALIE.fields().toList())
+            .from(POIDS_ANOMALIE)
+            .join(ANOMALIE)
+            .on(ANOMALIE.ID.eq(POIDS_ANOMALIE.ANOMALIE_ID))
+            .join(ANOMALIE_CATEGORIE)
+            .on(ANOMALIE.ANOMALIE_CATEGORIE_ID.eq(ANOMALIE_CATEGORIE.ID))
             .where(POIDS_ANOMALIE.ANOMALIE_ID.`in`(anomaliesIds))
             .and(POIDS_ANOMALIE.NATURE_ID.eq(natureId))
-            .and(typeVisite?.let { POIDS_ANOMALIE.TYPE_VISITE.contains(typeVisite) } ?: DSL.noCondition())
+            .and(
+                // Si anomalie système, elles n'ont pas de type de visites
+                typeVisite?.let { POIDS_ANOMALIE.TYPE_VISITE.contains(typeVisite) }?.or(
+                    ANOMALIE_CATEGORIE.CODE.eq(
+                        GlobalConstants.CATEGORIE_ANOMALIE_SYSTEME,
+                    ),
+                ) ?: DSL.noCondition(),
+            )
             .fetchInto()
 }
