193113564e169ea47916dc5bcd52499da1b977c0
Arthur <arj@atolcd.com>
2025-01-22 09:35:41 +0100

    Ajoute le type module à l'énumération en base.
    Issues : #240881
    Change-Id: I25fe98995a37474869d01f40f479d8a32e5dff9d

--- db/src/main/jooq/remocra/db/jooq/remocra/enums/TypeModule.kt
+++ db/src/main/jooq/remocra/db/jooq/remocra/enums/TypeModule.kt
@@ -37,6 +37,7 @@ enum class TypeModule(@get:JvmName("literal") public val literal: String) : Enum
     RAPPORT_PERSONNALISE("RAPPORT_PERSONNALISE"),
     DASHBOARD("DASHBOARD"),
     OPERATIONS_DIVERSES("OPERATIONS_DIVERSES"),
+    CRISE("CRISE"),
     ;
     override fun getCatalog(): Catalog? = schema.catalog
     override fun getSchema(): Schema = Remocra.REMOCRA
