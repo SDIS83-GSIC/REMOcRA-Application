bcd27de71d4a3f39c4b1a3f4a18151715f4ff5b2
Renaud KIEFFER <rki@atolcd.com>
2025-02-13 11:17:09 +0100

    Reprend le style "Attribution des fonctionnalités"
    Change-Id: I62b2419c485222537553d93dcbc0bd71bd2cb97a

--- frontend/src/pages/Admin/droit/LienProfilDroitList.tsx
+++ frontend/src/pages/Admin/droit/LienProfilDroitList.tsx
@@ -1,5 +1,5 @@
 import { object } from "yup";
-import { Button, Form, Table } from "react-bootstrap";
+import { Form, Table } from "react-bootstrap";
 import { useFormikContext } from "formik";
 import Container from "react-bootstrap/Container";
 import { useGet } from "../../../components/Fetch/useFetch.tsx";
@@ -14,6 +14,7 @@ import {
   TypeDroitLabel,
   TypeDroitSection,
 } from "../../../enums/DroitEnum.tsx";
+import SubmitFormButtons from "../../../components/Form/SubmitFormButtons.tsx";
 
 const LienProfilDroitList = () => {
   const lienProfilDroitListState = useGet(url`/api/lien-profil-droit`);
@@ -48,12 +49,10 @@ const LienProfilInner = ({ typeDroitList }: { typeDroitList: any[] }) => {
         icon={<IconUtilisateurs />}
         right={
           <>
-            <Button variant={"secondary"} onClick={() => resetForm()}>
-              Annuler
-            </Button>
-            <Button type="submit" variant={"info"}>
-              Valider
-            </Button>
+            <SubmitFormButtons
+              onSecondaryActionClick={() => resetForm()}
+              secondaryActionTitle={"Annuler"}
+            />
           </>
         }
       />
@@ -72,123 +71,128 @@ const LienProfilInner = ({ typeDroitList }: { typeDroitList: any[] }) => {
           </tr>
         </thead>
         <tbody>
-          {Object.values(SECTION_DROIT)
-            .filter((key) => SECTION_DROIT[key])
-            .map((currentSection) => {
-              return (
-                <>
-                  <tr>
-                    <td colSpan="6">{currentSection}</td>
-                  </tr>
-                  {TypeDroitSection.entries()
-                    .filter(([, value]) => value === currentSection)
-                    .map(([key]) => {
-                      return (
-                        <>
-                          {typeDroitList
-                            .filter((typeDroit) => key === typeDroit)
-                            .map((typeDroit, idxTD) => {
-                              return (
-                                <tr key={idxTD}>
-                                  <th>{typeDroit}</th>
-                                  <td>
-                                    {TypeDroitLabel.get(typeDroit)
-                                      ? TypeDroitLabel.get(typeDroit)
-                                      : ""}{" "}
-                                  </td>
-                                  {values.map((profilDroit, idxPD) => (
-                                    <td key={`${idxTD}${idxPD}`}>
-                                      <Form.Check
+          {Object.values(SECTION_DROIT).map((currentSection) => {
+            return (
+              <>
+                <tr>
+                  <td
+                    colSpan="6"
+                    className={"bg-primary text-light text-center fw-bolder"}
+                  >
+                    {currentSection}
+                  </td>
+                </tr>
+                {TypeDroitSection.entries()
+                  .filter(([, value]) => value === currentSection)
+                  .map(([key]) => {
+                    return (
+                      <>
+                        {typeDroitList
+                          .filter((typeDroit) => key === typeDroit)
+                          .map((typeDroit, idxTD) => {
+                            return (
+                              <tr key={idxTD}>
+                                <th>{typeDroit}</th>
+                                <td>
+                                  {TypeDroitLabel.get(typeDroit)
+                                    ? TypeDroitLabel.get(typeDroit)
+                                    : ""}{" "}
+                                </td>
+                                {values.map((profilDroit, idxPD) => (
+                                  <td
+                                    key={`${idxTD}${idxPD}`}
+                                    className={"text-center"}
+                                  >
+                                    <Form.Check
+                                      type={"checkbox"}
+                                      id={`check-${idxTD}-${idxPD}`}
+                                    >
+                                      <Form.Check.Input
                                         type={"checkbox"}
-                                        id={`check-${idxTD}-${idxPD}`}
-                                      >
-                                        <Form.Check.Input
-                                          type={"checkbox"}
-                                          value={typeDroit}
-                                          checked={profilDroit.profilDroitDroits?.some(
-                                            (e) => e === typeDroit,
-                                          )}
-                                          onChange={(event) => {
-                                            const value =
-                                              event.currentTarget.value;
-                                            const arrayVal = [value];
-                                            const right = value.substring(
-                                              value.lastIndexOf("_") + 1,
-                                            );
-                                            const left = value.substring(
-                                              0,
-                                              value.lastIndexOf("_"),
-                                            );
-                                            if (event.currentTarget.checked) {
-                                              // si un droit _A est coché, on rajoute les droits équivalents
-                                              if (right === "A") {
-                                                typeDroitList.forEach(
-                                                  (typeDroit) => {
-                                                    const substr =
-                                                      typeDroit.substring(
-                                                        0,
-                                                        value.lastIndexOf("_"),
-                                                      );
-                                                    if (
-                                                      left === substr &&
-                                                      values[
-                                                        idxPD
-                                                      ].profilDroitDroits.indexOf(
-                                                        typeDroit,
-                                                      ) === -1
-                                                    ) {
-                                                      arrayVal.push(typeDroit);
-                                                    }
-                                                  },
-                                                );
-                                              }
-                                              setFieldValue(
-                                                `${idxPD}.profilDroitDroits`,
-                                                [
-                                                  ...values[idxPD]
-                                                    .profilDroitDroits,
-                                                  ...arrayVal,
-                                                ],
-                                              );
-                                            } else {
-                                              // si un droit _CRUD est décoché, on tente de retirer le droit _A s'il existe
-                                              if (
-                                                "CRUD"
-                                                  .split("")
-                                                  .some(
-                                                    (action) =>
-                                                      action === right,
-                                                  )
-                                              ) {
-                                                arrayVal.push(`${left}_A`);
-                                              }
-                                              setFieldValue(
-                                                `${idxPD}.profilDroitDroits`,
-                                                values[
-                                                  idxPD
-                                                ].profilDroitDroits?.filter(
-                                                  (v) =>
-                                                    v &&
-                                                    !arrayVal.some(
-                                                      (a) => v === a,
-                                                    ),
-                                                ),
+                                        value={typeDroit}
+                                        checked={profilDroit.profilDroitDroits?.some(
+                                          (e) => e === typeDroit,
+                                        )}
+                                        onChange={(event) => {
+                                          const value =
+                                            event.currentTarget.value;
+                                          const arrayVal = [value];
+                                          const right = value.substring(
+                                            value.lastIndexOf("_") + 1,
+                                          );
+                                          const left = value.substring(
+                                            0,
+                                            value.lastIndexOf("_"),
+                                          );
+                                          if (event.currentTarget.checked) {
+                                            // si un droit _A est coché, on rajoute les droits équivalents
+                                            if (right === "A") {
+                                              typeDroitList.forEach(
+                                                (typeDroit) => {
+                                                  const substr =
+                                                    typeDroit.substring(
+                                                      0,
+                                                      value.lastIndexOf("_"),
+                                                    );
+                                                  if (
+                                                    left === substr &&
+                                                    values[
+                                                      idxPD
+                                                    ].profilDroitDroits.indexOf(
+                                                      typeDroit,
+                                                    ) === -1
+                                                  ) {
+                                                    arrayVal.push(typeDroit);
+                                                  }
+                                                },
                                               );
                                             }
-                                          }}
-                                        />
-                                      </Form.Check>
-                                    </td>
-                                  ))}
-                                </tr>
-                              );
-                            })}
-                        </>
-                      );
-                    })}
-                </>
-              );
-            })}
+                                            setFieldValue(
+                                              `${idxPD}.profilDroitDroits`,
+                                              [
+                                                ...values[idxPD]
+                                                  .profilDroitDroits,
+                                                ...arrayVal,
+                                              ],
+                                            );
+                                          } else {
+                                            // si un droit _CRUD est décoché, on tente de retirer le droit _A s'il existe
+                                            if (
+                                              "CRUD"
+                                                .split("")
+                                                .some(
+                                                  (action) => action === right,
+                                                )
+                                            ) {
+                                              arrayVal.push(`${left}_A`);
+                                            }
+                                            setFieldValue(
+                                              `${idxPD}.profilDroitDroits`,
+                                              values[
+                                                idxPD
+                                              ].profilDroitDroits?.filter(
+                                                (v) =>
+                                                  v &&
+                                                  !arrayVal.some(
+                                                    (a) => v === a,
+                                                  ),
+                                              ),
+                                            );
+                                          }
+                                        }}
+                                      />
+                                    </Form.Check>
+                                  </td>
+                                ))}
+                              </tr>
+                            );
+                          })}
+                      </>
+                    );
+                  })}
+              </>
+            );
+          })}
         </tbody>
       </Table>
     </FormContainer>
