e4a4fc04384128a2b118f53ccf202e4244b4699b
Thomas Broyer <tbr@atolcd.com>
2025-02-17 14:53:38 +0100

    Ajoute Glowroot dans l'image Docker
    Change-Id: I6abd641a688105cc99e0b53e591af26a33065e23

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,6 +3,13 @@
 case "$1" in
   # Argument direct, ou sous-commandes connues gérées par l’application
   -* | serve | migrate-db | validate-db | info-db | repair-db )
+    if [ -n "$GLOWROOT_ENABLED" ] && [ "$GLOWROOT_ENABLED" != "false" ] && [ "$GLOWROOT_ENABLED" != "0" ]; then
+      JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/glowroot/glowroot.jar"
+      if [ -n "$GLOWROOT_PORT" ]; then
+        JAVA_OPTS="$JAVA_OPTS -Dglowroot.agent.port=$GLOWROOT_PORT"
+      fi
+    fi
+
     # shellcheck disable=SC2206 # pour GC_OPTS et JAVA_OPTS
     commande=( java $GC_OPTS $JAVA_OPTS -cp "/opt/remocra/classes:/opt/remocra/libs/*" remocra.cli.Main )
     if [[ "$(id -u)" == "0" ]]; then
