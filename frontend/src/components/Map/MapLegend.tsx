8d898c726c66879f07ba9f17a9057b8cdc12e9e4
Clément GRENOT <cgr@atolcd.com>
2025-02-19 09:54:30 +0100

    Met la légende des couches dans un accordéon
    Issue: #243922
    Change-Id: I08a5c23b7563d1a9c6953e2b37b9074d38c5f363

--- frontend/src/components/Map/MapLegend.tsx
+++ frontend/src/components/Map/MapLegend.tsx
@@ -1,11 +1,12 @@
-import { forwardRef, useImperativeHandle, useState } from "react";
-import { Badge, Card, Collapse, Form, Image, ListGroup } from "react-bootstrap";
-import FormRange from "react-bootstrap/FormRange";
 import { getUid } from "ol";
+import { forwardRef, ReactNode, useImperativeHandle, useState } from "react";
+import { Badge, Collapse, Form, Image, ListGroup } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
+import FormRange from "react-bootstrap/FormRange";
 import Row from "react-bootstrap/Row";
-import "./legend.css";
+import AccordionCustom, { useAccordionState } from "../Accordion/Accordion.tsx";
 import { IconMinus, IconPlus } from "../Icon/Icon.tsx";
+import "./legend.css";
 
 const MapLegend = forwardRef(
   (
@@ -17,6 +18,7 @@ const MapLegend = forwardRef(
   ) => {
     const [activeLayers, setActiveLayers] = useState([]);
     const [open, setOpen] = useState({});
+    const { handleShowClose, activesKeys } = useAccordionState([false]); // Tous les volets fermés par défaut
 
     useImperativeHandle(ref, () => ({
       addActiveLayer(layerId) {
@@ -31,102 +33,110 @@ const MapLegend = forwardRef(
       return activeLayers.some((l) => getUid(layer.openlayer) === l);
     }
 
-    return (
-      <>
-        {layers
-          ?.filter((g) => g.layers.length > 0)
-          .map((group: any, groupIdx: number) => (
-            <Card key={groupIdx}>
-              <Card.Header>{group.libelle}</Card.Header>
-              <ListGroup className="list-group-flush">
-                {group.layers
-                  .sort((a, b) => {
-                    return b.ordre - a.ordre;
-                  })
-                  .map((layer: any, layerIdx: number) => (
-                    <ListGroup.Item
-                      key={layerIdx}
-                      className={isChecked(layer) ? "" : "noprint"}
-                    >
-                      <Row>
-                        <Col className={"icone-legende"}>
-                          {layer.icone && (
-                            <Image src={layer.icone} rounded fluid />
-                          )}
-                        </Col>
-                        <Col>
-                          <Form.Group controlId={getUid(layer.openlayer)}>
-                            <Row>
-                              <Col>
-                                <Form.Switch
-                                  checked={isChecked(layer)}
-                                  onClick={() => {
-                                    addOrRemoveLayer(layer);
-                                  }}
-                                  label={
-                                    <>
-                                      {layer.libelle}
-                                      {layer.legende && (
-                                        <img src={layer.icone} height={24} />
-                                      )}
-                                    </>
-                                  }
-                                />
-                              </Col>
-                              {layer.legende && (
-                                <Col xs={"auto"}>
-                                  <Badge
-                                    pill
-                                    onClick={() =>
-                                      setOpen({
-                                        ...open,
-                                        ...{
-                                          [getUid(layer.openlayer)]:
-                                            !open[getUid(layer.openlayer)],
-                                        },
-                                      })
-                                    }
-                                  >
-                                    {open[getUid(layer.openlayer)] ? (
-                                      <IconMinus />
-                                    ) : (
-                                      <IconPlus />
+    const listeVoletsAccordion: { header: string; content: ReactNode }[] = [];
+
+    layers
+      ?.filter((g) => g.layers.length > 0)
+      .map((group) =>
+        listeVoletsAccordion.push({
+          header: group.libelle,
+          content: (
+            <ListGroup className="list-group-flush">
+              {group.layers
+                .sort((a, b) => {
+                  return b.ordre - a.ordre;
+                })
+                .map((layer: any, layerIdx: number) => (
+                  <ListGroup.Item
+                    key={layerIdx}
+                    className={isChecked(layer) ? "" : "noprint"}
+                  >
+                    <Row>
+                      <Col className={"icone-legende"}>
+                        {layer.icone && (
+                          <Image src={layer.icone} rounded fluid />
+                        )}
+                      </Col>
+                      <Col>
+                        <Form.Group controlId={getUid(layer.openlayer)}>
+                          <Row>
+                            <Col>
+                              <Form.Switch
+                                checked={isChecked(layer)}
+                                onClick={() => {
+                                  addOrRemoveLayer(layer);
+                                }}
+                                label={
+                                  <>
+                                    {layer.libelle}
+                                    {layer.legende && (
+                                      <img src={layer.icone} height={24} />
                                     )}
-                                  </Badge>
-                                </Col>
-                              )}
-                            </Row>
-                          </Form.Group>
-                          <FormRange
-                            className={"noprint"}
-                            disabled={!isChecked(layer)}
-                            min={0}
-                            max={1}
-                            step={0.1}
-                            defaultValue={layer.openlayer.getOpacity()}
-                            onChange={(evt) => {
-                              layer.openlayer.setOpacity(
-                                parseFloat(evt.target.value),
-                              );
-                            }}
-                          />
+                                  </>
+                                }
+                              />
+                            </Col>
+                            {layer.legende && (
+                              <Col xs={"auto"}>
+                                <Badge
+                                  pill
+                                  onClick={() =>
+                                    setOpen({
+                                      ...open,
+                                      ...{
+                                        [getUid(layer.openlayer)]:
+                                          !open[getUid(layer.openlayer)],
+                                      },
+                                    })
+                                  }
+                                >
+                                  {open[getUid(layer.openlayer)] ? (
+                                    <IconMinus />
+                                  ) : (
+                                    <IconPlus />
+                                  )}
+                                </Badge>
+                              </Col>
+                            )}
+                          </Row>
+                        </Form.Group>
+                        <FormRange
+                          className={"noprint"}
+                          disabled={!isChecked(layer)}
+                          min={0}
+                          max={1}
+                          step={0.1}
+                          defaultValue={layer.openlayer.getOpacity()}
+                          onChange={(evt) => {
+                            layer.openlayer.setOpacity(
+                              parseFloat(evt.target.value),
+                            );
+                          }}
+                        />
+                      </Col>
+                    </Row>
+                    {layer.legende && (
+                      <Row>
+                        <Col>
+                          <Collapse in={open[getUid(layer.openlayer)]}>
+                            <Image src={layer.legende} rounded fluid />
+                          </Collapse>
                         </Col>
                       </Row>
-                      {layer.legende && (
-                        <Row>
-                          <Col>
-                            <Collapse in={open[getUid(layer.openlayer)]}>
-                              <Image src={layer.legende} rounded fluid />
-                            </Collapse>
-                          </Col>
-                        </Row>
-                      )}
-                    </ListGroup.Item>
-                  ))}
-              </ListGroup>
-            </Card>
-          ))}
-      </>
+                    )}
+                  </ListGroup.Item>
+                ))}
+            </ListGroup>
+          ),
+        }),
+      );
+
+    return (
+      <AccordionCustom
+        activesKeys={activesKeys}
+        handleShowClose={handleShowClose}
+        list={listeVoletsAccordion}
+      />
     );
   },
 );
