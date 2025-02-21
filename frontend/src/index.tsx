15fa9d42ea34fc8a77b773e9d73d2af0f3b433c4
Thomas Broyer <tbr@atolcd.com>
2025-02-21 16:09:08 +0100

    Rend "npm run watch" utilisable à nouveau
    Désactive (temporairement ?) le React.StrictMode qui est
    incompatible (depuis React 18) avec react-async (qui n'a
    d'ailleurs reçu aucune mise à jour depuis 2021).
    
    Change-Id: I886e59e49d0203eec488b8e40327aa3dea2e5d4d

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./style.scss";
 
+// FIXME: remettre en place <StrictMode> quand on n'utilisera plus react-async
+// cf. src/components/Fetch/useFetch.tsx
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
 );
