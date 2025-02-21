0b75d203ef64bc458da7472d29891393ffac8983
Renaud KIEFFER <rki@atolcd.com>
2025-02-21 10:45:36 +0100

    Place tous les titres de page au même endroit
    Issue: #243065
    
    Change-Id: I0a143cd48bc50714d12ff8434b6dd56783d7e9d0

--- frontend/src/components/Elements/PageTitle/PageTitle.tsx
+++ frontend/src/components/Elements/PageTitle/PageTitle.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { Button, Col, Row } from "react-bootstrap";
+import { Button, Col, Row, Container } from "react-bootstrap";
 import { useLocation, useNavigate } from "react-router-dom";
 import { URLS } from "../../../routes.tsx";
 import { IconPreviousPage } from "../../Icon/Icon.tsx";
@@ -22,47 +22,49 @@ const PageTitle = ({
   const navigate = useNavigate();
   const location = useLocation();
   return (
-    <Row className="my-3 mx-2 noprint">
-      <Col>
-        {displayReturnButton && (
-          <Row className="d-flex flex-row py-3">
+    <Container>
+      <Row className="my-3 mx-2 noprint">
+        <Col>
+          {displayReturnButton && (
+            <Row className="d-flex flex-row py-3">
+              <Col>
+                <Button
+                  variant={"link"}
+                  className="text-decoration-none text-sm font-weight-bold"
+                  onClick={() => {
+                    if (location.state?.from?.slice(-1)[0]) {
+                      navigate(location.state.from.slice(-1)[0], {
+                        state: { from: location.state.from.slice(0, -1) },
+                      });
+                    } else if (urlRetour) {
+                      navigate(urlRetour);
+                    } else {
+                      navigate(URLS.ACCUEIL);
+                    }
+                  }}
+                >
+                  <IconPreviousPage /> Retour
+                </Button>
+              </Col>
+            </Row>
+          )}
+          <Row>
             <Col>
-              <Button
-                variant={"link"}
-                className="text-decoration-none text-sm font-weight-bold"
-                onClick={() => {
-                  if (location.state?.from?.slice(-1)[0]) {
-                    navigate(location.state.from.slice(-1)[0], {
-                      state: { from: location.state.from.slice(0, -1) },
-                    });
-                  } else if (urlRetour) {
-                    navigate(urlRetour);
-                  } else {
-                    navigate(URLS.ACCUEIL);
-                  }
-                }}
-              >
-                <IconPreviousPage /> Retour
-              </Button>
+              <h1 className="fw-bold">
+                {icon} {title}
+              </h1>
             </Col>
           </Row>
-        )}
-        <Row>
-          <Col>
-            <h1 className="fw-bold">
-              {icon} {title}
-            </h1>
-          </Col>
-        </Row>
-      </Col>
-      {right ? (
-        <Col className="my-auto" sm={"auto"}>
-          {right}
         </Col>
-      ) : (
-        ""
-      )}
-    </Row>
+        {right ? (
+          <Col className="my-auto" sm={"auto"}>
+            {right}
+          </Col>
+        ) : (
+          ""
+        )}
+      </Row>
+    </Container>
   );
 };
 
