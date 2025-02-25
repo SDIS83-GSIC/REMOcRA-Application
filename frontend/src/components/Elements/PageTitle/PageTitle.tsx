310fb6b3340f252fd91c656d4f2957c1f16cd239
Renaud KIEFFER <rki@atolcd.com>
2025-02-25 11:00:34 +0100

    Réduit l'espace du PageTitle
    Change-Id: I95b7962d7b8fce598743a1faee8326f33f2164a7

--- frontend/src/components/Elements/PageTitle/PageTitle.tsx
+++ frontend/src/components/Elements/PageTitle/PageTitle.tsx
@@ -22,42 +22,36 @@ const PageTitle = ({
   const navigate = useNavigate();
   const location = useLocation();
   return (
-    <Container>
-      <Row className="my-3 mx-2 noprint">
-        <Col>
-          {displayReturnButton && (
-            <Row className="d-flex flex-row py-3">
-              <Col>
-                <Button
-                  variant={"link"}
-                  className="text-decoration-none text-sm font-weight-bold"
-                  onClick={() => {
-                    if (location.state?.from?.slice(-1)[0]) {
-                      navigate(location.state.from.slice(-1)[0], {
-                        state: { from: location.state.from.slice(0, -1) },
-                      });
-                    } else if (urlRetour) {
-                      navigate(urlRetour);
-                    } else {
-                      navigate(URLS.ACCUEIL);
-                    }
-                  }}
-                >
-                  <IconPreviousPage /> Retour
-                </Button>
-              </Col>
-            </Row>
-          )}
-          <Row>
-            <Col>
-              <h1 className="fw-bold">
-                {icon} {title}
-              </h1>
-            </Col>
-          </Row>
+    <Container className="ps-0">
+      <Row className="mb-4 mt-3 mx-0 ps-0 noprint">
+        {displayReturnButton && (
+          <Col xs={"auto"} className={"ps-0"}>
+            <Button
+              variant={"link"}
+              className="text-decoration-none text-sm font-weight-bold ps-0"
+              onClick={() => {
+                if (location.state?.from?.slice(-1)[0]) {
+                  navigate(location.state.from.slice(-1)[0], {
+                    state: { from: location.state.from.slice(0, -1) },
+                  });
+                } else if (urlRetour) {
+                  navigate(urlRetour);
+                } else {
+                  navigate(URLS.ACCUEIL);
+                }
+              }}
+            >
+              <IconPreviousPage /> Retour
+            </Button>
+          </Col>
+        )}
+        <Col xs={"auto"}>
+          <h1 className="fw-bold">
+            {icon} {title}
+          </h1>
         </Col>
         {right ? (
-          <Col className="my-auto" sm={"auto"}>
+          <Col className="ms-auto" sm={"auto"}>
             {right}
           </Col>
         ) : (
