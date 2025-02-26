a118d66e8d8f0ca42f7ce5d0e7ac741ab962616d
Renaud KIEFFER <rki@atolcd.com>
2025-02-26 14:49:52 +0100

    Ajoute le squelette de page au "NoMatch"
    Change-Id: I47f5a2a2ab9a9a2cf0cd4cebe1873a5641bc5e7a

--- frontend/src/components/Router/NoMatch.tsx
+++ frontend/src/components/Router/NoMatch.tsx
@@ -1,5 +1,12 @@
+import SquelettePage from "../../pages/SquelettePage.tsx";
+import Header from "../../components/Header/Header.tsx";
+
 const NoMatch = () => {
-  return <div>La page que vous demandez n&apos;a pas été trouvée.</div>;
+  return (
+    <SquelettePage navbar={<Header />} banner>
+      <div>La page que vous demandez n&apos;a pas été trouvée.</div>
+    </SquelettePage>
+  );
 };
 
 export default NoMatch;
