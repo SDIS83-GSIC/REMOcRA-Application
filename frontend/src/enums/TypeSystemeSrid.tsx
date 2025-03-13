f2110cc81369a3216ad79b8dee4ad327f72e2f1e
Thibault DeGivry <tde@atolcd.com>
2025-03-13 11:53:55 +0100

    Complète la liste de SRID côté front
    Ajoute le code 3857 global et 32620 spécifique Guadeloupe
    
    Change-Id: I35894f8fe47cbb6c1f61f721223618b01245ee03

--- frontend/src/enums/TypeSystemeSrid.tsx
+++ frontend/src/enums/TypeSystemeSrid.tsx
@@ -1,8 +1,10 @@
 const TypeSystemeSrid = [
-  { srid: 2154, nomSystem: "Lambert 93", actif: false },
-  { srid: 2972, nomSystem: "RGFG95", actif: false },
-  { srid: 4326, nomSystem: "WGS84 degrés décimaux", actif: true },
-  { srid: -1, nomSystem: "WGS84 degrés sexagésimaux", actif: true },
+  { srid: 2154, nomSystem: "RGF93 v1 / Lambert-93", actif: false },
+  { srid: 2972, nomSystem: "RGFG95 / UTM zone 22N", actif: false },
+  { srid: 3857, nomSystem: "WGS 84 / Pseudo-Mercator", actif: true },
+  { srid: 4326, nomSystem: "WGS 84 degrés décimaux", actif: true },
+  { srid: 32620, nomSystem: "WGS 84 / UTM zone 20N", actif: false },
+  { srid: -1, nomSystem: "WGS 84 degrés sexagésimaux", actif: true },
 ] as const;
 
 export default TypeSystemeSrid;
