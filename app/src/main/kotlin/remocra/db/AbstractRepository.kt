becdbc484df2688ef6eb15f0a64af0988fabaaea
Hugo Bole <hbo@atolcd.com>
2025-06-24 12:57:40 +0200

    Ajoute une fonction "équivalente" à unaccent dans les repo
    Change-Id: I8a3188fad15a1d5c655a80b35887bba4ff3c97da

--- app/src/main/kotlin/remocra/db/AbstractRepository.kt
+++ app/src/main/kotlin/remocra/db/AbstractRepository.kt
@@ -3,6 +3,7 @@ package remocra.db
 import com.google.inject.Inject
 import remocra.app.AppSettings
 import remocra.utils.DateUtils
+import java.text.Normalizer
 
 abstract class AbstractRepository {
     @Inject
@@ -17,3 +18,9 @@ abstract class AbstractRepository {
     protected val SRID: Int
         get() = appSettings.srid
 }
+
+/**
+ * Equivalent à la primitive "unaccent" de postgres, enfin en théorie, à vérifier au fur et à mesure. Objectif : permettre de faire un sortBy après récupération des données, dans le même ordre que ce que fournirait PG.
+ */
+public fun String.unaccent(): String = Normalizer.normalize(this, Normalizer.Form.NFD)
+    .replace("\\p{Mn}+".toRegex(), "")
