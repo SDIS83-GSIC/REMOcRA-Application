fd77d46d4723e953ee3fbde7e612f7768a8fb2d3
Thomas Broyer <tbr@atolcd.com>
2025-07-20 16:35:51 +0200

    Simplifie la configuration flyway/jooq dans le build
    …en utilisant les plugins net.ltgt.flyway et net.ltgt.jooq-kotlin
    
    Change-Id: Id0db409caaf7cf9f70c4c9ba13333430f4f2f960

--- db/build.gradle.kts
+++ db/build.gradle.kts
@@ -1,39 +1,22 @@
-import org.flywaydb.gradle.task.FlywayMigrateTask
-import java.util.Properties
-
-buildscript {
-    dependencies {
-        classpath(libs.flyway.database.postgresql) { because("Requis par le plugin Flyway") }
-        classpath(libs.postgresql) { because("Requis par le plugin Flyway") }
-    }
-}
+import net.ltgt.gradle.flyway.tasks.FlywayMigrate
 
 plugins {
     id("local.kotlin-base")
     kotlin("jvm")
     idea
     alias(libs.plugins.flyway)
+    alias(libs.plugins.jooq)
 }
 
-val flywayConf by lazy {
-    Properties().apply {
-        file("src/main/resources/db/flyway.conf").reader().use { load(it) }
-    }
-}
-
-val dbUrl = providers.gradleProperty("db.url").getOrElse("jdbc:postgresql://localhost:5432/remocra")
-val dbUser = providers.gradleProperty("db.user").getOrElse("remocra")
-val dbPassword = providers.gradleProperty("db.password").getOrElse("remocra")
+val dbUrl = providers.gradleProperty("db.url").orElse("jdbc:postgresql://localhost:5432/remocra")
+val dbUser = providers.gradleProperty("db.user").orElse("remocra")
+val dbPassword = providers.gradleProperty("db.password").orElse("remocra")
 
 flyway {
     url = dbUrl
     user = dbUser
     password = dbPassword
-    schemas = flywayConf.getProperty("flyway.schemas").split(',').toTypedArray()
-    cleanDisabled = flywayConf.getProperty("flyway.cleanDisabled").toBoolean()
 }
-
-val jooqCodegen: Configuration by configurations.creating
 
 dependencies {
     api(libs.jooq.kotlin)
@@ -44,60 +27,34 @@ dependencies {
     runtimeOnly(libs.postgresql)
     runtimeOnly(libs.flyway.database.postgresql)
 
+    flyway(libs.postgresql)
+    flyway(libs.flyway.core)
+    flyway(libs.flyway.database.postgresql)
+
     jooqCodegen(libs.jooq.codegen)
     jooqCodegen(libs.postgresql)
 }
 
-val jooqOutputDir = file("src/main/jooq")
-
 tasks {
-    val flywayMigrate by existing
+    register<FlywayMigrate>("flywayMigrateData") {
+        migrationLocations.setFrom(file("src/data"))
 
-    register<FlywayMigrateTask>("flywayMigrateData") {
-        table = "flyway_data_history"
-        locations = arrayOf("filesystem:${file("src/data")}")
-        baselineOnMigrate = true
-        baselineVersion = "0"
+        configuration.put("flyway.table", "flyway_data_history")
+        configuration.put("flyway.baselineOnMigrate", "true")
+        configuration.put("flyway.baselineVersion", "0")
 
         dependsOn(flywayMigrate)
     }
-    val jooq by registering(JavaExec::class) {
-        val jooqConfigFile = file("src/jooq-codegen.xml")
-
+    jooq {
         dependsOn(flywayMigrate)
         finalizedBy(spotlessKotlinApply)
 
-        inputs.dir("src/main/resources/db/migration").withPathSensitivity(PathSensitivity.RELATIVE)
-        inputs.file(jooqConfigFile).withPathSensitivity(PathSensitivity.NONE)
-        outputs.dir(jooqOutputDir)
-
-        doFirst {
-            project.delete(jooqOutputDir)
-        }
-        classpath = jooqCodegen
-        mainClass = "org.jooq.codegen.GenerationTool"
-        systemProperties = mapOf(
-            "db.url" to dbUrl,
-            "db.user" to dbUser,
-            "db.password" to dbPassword,
-            "outputdir" to jooqOutputDir.path,
-        )
-        args(jooqConfigFile)
+        url = dbUrl
+        user = dbUser
+        password = dbPassword
     }
     spotlessKotlin {
         mustRunAfter(jooq)
-    }
-}
-sourceSets {
-    main {
-        java {
-            srcDir(jooqOutputDir)
-        }
-    }
-}
-idea {
-    module {
-        generatedSourceDirs.add(jooqOutputDir)
     }
 }
 
