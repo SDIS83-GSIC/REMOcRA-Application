26f7cfd19ca34e62fa6ee1614433ee8c48d02cbf
mbe <mbe@atolcd.com>
2025-11-07 10:54:57 +0100

    Ajoute les natures DECI ICPE et ICPE Conventionné
    Issue: #269674
    Change-Id: I592a78a292c45412f5536e35ca75354b8e99cb0e

---
+++ db/src/main/resources/db/migration/V171__.sql
@@ -0,0 +1,3 @@
+INSERT INTO remocra.nature_deci (nature_deci_id, nature_deci_actif, nature_deci_code, nature_deci_libelle, nature_deci_protected)
+VALUES(gen_random_uuid(), true, 'ICPE', 'ICPE', true),
+(gen_random_uuid(), true, 'ICPE_CONVENTIONNE', 'ICPE Conventionné', true);
