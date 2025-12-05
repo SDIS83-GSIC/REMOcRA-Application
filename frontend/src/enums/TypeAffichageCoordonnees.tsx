02d95a002b291db138104a55047554fc5553d82d
mbe <mbe@atolcd.com>
2025-12-05 16:22:02 +0100

    Ajoute les degrés sexagesimaux dans les parametres carto
    Issue: #273742
    Change-Id: Ib8eb41dabaa9bd35868ed2310d4e7098ea1055cf

--- frontend/src/enums/TypeAffichageCoordonnees.tsx
+++ frontend/src/enums/TypeAffichageCoordonnees.tsx
@@ -7,11 +7,16 @@ const typeAffichageCoordonnees = [
     id: "DEGRES_DECIMAUX",
     libelle: "Dégrés décimaux",
   },
+  {
+    id: "DEGRES_SEXAGESIMAUX",
+    libelle: "Dégrés sexagésimaux",
+  },
 ];
 
 export const enum TYPE_AFFICHAGE_COORDONNEES {
   X_Y = "X_Y",
   DEGRES_DECIMAUX = "DEGRES_DECIMAUX",
+  DEGRES_SEXAGESIMAUX = "DEGRES_SEXAGESIMAUX",
 }
 
 export default typeAffichageCoordonnees;
