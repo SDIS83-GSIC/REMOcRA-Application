a03ed8309ca42390de1c6edc78bb369306b42410
Xavier Calland <xca@atolcd.com>
2025-12-17 15:14:59 +0100

    Ajoute l'agent OpenTelemetry dans les images Docker
    Pour l'application et GeoServer
    
    Change-Id: Ie698f0a46666c72192ffa236d4a1c437424b6ffd

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -9,12 +9,27 @@ case "$1" in
         JAVA_OPTS="$JAVA_OPTS -Dglowroot.agent.port=${GLOWROOT_PORT:-4000}"
       fi
     fi
-
-     # shellcheck disable=SC2206 # pour GC_OPTS, GC_LOG_OPTS, JAVA_OPTS et JAVA_MEM_OPTS
-    commande=( java $GC_OPTS $GC_LOG_OPTS $JAVA_OPTS $JAVA_MEM_OPTS -cp "/opt/remocra/classes:/opt/remocra/libs/*" remocra.cli.Main )
+    if [ "${OTEL_JAVAAGENT_ENABLED,,}" = "true" ]; then
+      OTEL_RESOURCE_ATTRIBUTES="service.namespace=$OTEL_SERVICE_NAMESPACE,service.instance.id=$OTEL_SERVICE_INSTANCE_ID,service.version=$OTEL_SERVICE_VERSION,$OTEL_RESOURCE_ATTRIBUTES"
+      echo "---------------------------------"
+      echo "OpenTelemetry environment variables :"
+      env | grep "^OTEL_" | sort
+      echo "---------------------------------"
+      OTEL_OPTS="-javaagent:/opt/opentelemetry-javaagent/opentelemetry-javaagent.jar"
+    fi
+     # shellcheck disable=SC2206 # pour GC_OPTS, GC_LOG_OPTS, JAVA_OPTS, JAVA_MEM_OPTS et OTEL_OPTS
+    commande=( java $GC_OPTS $GC_LOG_OPTS $JAVA_OPTS $JAVA_MEM_OPTS $OTEL_OPTS -cp "/opt/remocra/classes:/opt/remocra/libs/*" remocra.cli.Main )
     if [[ "$(id -u)" == "0" ]]; then
       commande=( setpriv --reuid=remocra --regid=remocra --init-groups "${commande[@]}" )
     fi
+    echo "---------------------------------"
+    echo "Starting with JVM options :"
+    echo "GC_OPTS:         $GC_OPTS"
+    echo "GC_LOG_OPTS:     $GC_LOG_OPTS"
+    echo "JAVA_OPTS:       $JAVA_OPTS"
+    echo "JAVA_MEM_OPTS:   $JAVA_MEM_OPTS"
+    echo "OTEL_OPTS:       $OTEL_OPTS"
+    echo "---------------------------------"
     exec "${commande[@]}" "$@"
     ;;
   * )
--- geoserver/entrypoint.sh
+++ geoserver/entrypoint.sh
@@ -14,12 +14,24 @@ case "$1" in
         JAVA_OPTS="$JAVA_OPTS -Dglowroot.agent.port=${GEOSERVER_GLOWROOT_PORT:-4001}"
       fi
     fi
+    if [ "${OTEL_JAVAAGENT_ENABLED,,}" = "true" ]; then
+      OTEL_RESOURCE_ATTRIBUTES="service.namespace=$OTEL_SERVICE_NAMESPACE,service.instance.id=$OTEL_SERVICE_INSTANCE_ID,service.version=$OTEL_SERVICE_VERSION,$OTEL_RESOURCE_ATTRIBUTES"
+      echo "---------------------------------"
+      echo "OpenTelemetry environment variables :"
+      env | grep "^OTEL_" | sort
+      echo "---------------------------------"
+      JAVA_OPTS="$JAVA_OPTS -javaagent:/opt/opentelemetry-javaagent/opentelemetry-javaagent.jar"
+    fi
 
     export JAVA_OPTS="$JAVA_OPTS -Djetty.http.port=${GEOSERVER_PORT:-8090}"
     commande=( /opt/geoserver/bin/startup.sh )
     if [[ "$(id -u)" == "0" ]]; then
       commande=( setpriv --reuid=geoserver --regid=geoserver --init-groups "${commande[@]}" )
     fi
+    echo "---------------------------------"
+    echo "Starting with JVM options :"
+    echo "JAVA_OPTS:       $JAVA_OPTS"
+    echo "---------------------------------"
     exec "${commande[@]}" "$@"
     ;;
   load-data )
