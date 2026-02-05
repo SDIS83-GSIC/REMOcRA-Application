e7bd0e2172227abd34bf97a7eadfdd8e3deea798
mbe <mbe@atolcd.com>
2026-02-05 14:30:19 +0100

    Améliore la saisie des coordonnées pour les aires d'aspirations déportées
    Issue: #279533
    Change-Id: I923d29cb0f2315ca75d463a7adf1dfdd32c2769c

--- frontend/src/pages/Pena/AireAspiration.tsx
+++ frontend/src/pages/Pena/AireAspiration.tsx
@@ -1,5 +1,6 @@
 import { useFormikContext } from "formik";
 import { WKT } from "ol/format";
+import { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { object } from "yup";
@@ -10,22 +11,27 @@ import { useGet } from "../../components/Fetch/useFetch.tsx";
 import {
   CheckBoxInput,
   FormContainer,
-  NumberInput,
   TextInput,
 } from "../../components/Form/Form.tsx";
 import MyFormik from "../../components/Form/MyFormik.tsx";
 import SelectForm from "../../components/Form/SelectForm.tsx";
 import SubmitFormButtons from "../../components/Form/SubmitFormButtons.tsx";
 import { IconAireAspiration } from "../../components/Icon/Icon.tsx";
+import TypeSystemeSrid from "../../enums/TypeSystemeSrid.tsx";
 import url from "../../module/fetch.tsx";
 import { URLS } from "../../routes.tsx";
 
 export const getInitialValues = (
   data: any,
+  srid: number,
 ): { listeAireAspiration: AireAspirationType[] } => ({
   listeAireAspiration: data.map((v: any) => {
     const geom = v.geometrie?.split(";");
     const geometry = geom ? new WKT().readGeometry(geom[1]) : undefined;
+    const coords = (geometry as any)?.getCoordinates?.();
+    const coordX = coords?.[0];
+    const coordY = coords?.[1];
+    const dataSrid = geom?.[0]?.split("=")?.pop();
     return {
       penaAspirationId: v.penaAspirationId,
       numero: v.numero,
@@ -33,9 +39,15 @@ export const getInitialValues = (
       hauteurSuperieure3Metres: v.hauteurSuperieure3Metres,
       typePenaAspirationId: v.typePenaAspirationId,
       estDeporte: v.estDeporte,
-      coordonneeX: geometry?.getCoordinates()[0],
-      coordonneeY: geometry?.getCoordinates()[1],
-      srid: geom?.[0]?.split("=")?.pop(),
+      coordonneeX: coordX,
+      coordonneeY: coordY,
+      srid: dataSrid ? parseInt(dataSrid) : srid,
+      typeSystemeSrid: dataSrid
+        ? (TypeSystemeSrid.find((e) => e.srid === parseInt(dataSrid))?.srid ??
+          TypeSystemeSrid[0].srid)
+        : TypeSystemeSrid[0].srid,
+      coordonneeXToDisplay: coordX,
+      coordonneeYToDisplay: coordY,
     };
   }),
 });
@@ -45,18 +57,26 @@ export const prepareVariables = (values: {
   listeAireAspiration: AireAspirationType[];
 }) => ({
   listeAireAspiration: values.listeAireAspiration.map(
-    (v: AireAspirationType) => ({
-      penaAspirationId: v.penaAspirationId,
-      numero: v.numero,
-      estNormalise: v.estNormalise,
-      hauteurSuperieure3Metres: v.hauteurSuperieure3Metres,
-      typePenaAspirationId: v.typePenaAspirationId,
-      estDeporte: v.estDeporte,
-      geometrie:
-        v.srid && v.coordonneeX
-          ? `SRID=${v.srid};POINT(${v.coordonneeX} ${v.coordonneeY})`
-          : undefined,
-    }),
+    (v: AireAspirationType) => {
+      const coordX = v.coordonneeX ? parseFloat(String(v.coordonneeX)) : null;
+      const coordY = v.coordonneeY ? parseFloat(String(v.coordonneeY)) : null;
+
+      const hasValidCoords =
+        coordX != null && !isNaN(coordX) && coordY != null && !isNaN(coordY);
+
+      return {
+        penaAspirationId: v.penaAspirationId,
+        numero: v.numero,
+        estNormalise: v.estNormalise,
+        hauteurSuperieure3Metres: v.hauteurSuperieure3Metres,
+        typePenaAspirationId: v.typePenaAspirationId,
+        estDeporte: v.estDeporte,
+        geometrie:
+          v.estDeporte && v.srid && hasValidCoords
+            ? `SRID=${v.srid};POINT(${coordX} ${coordY})`
+            : null,
+      };
+    },
   ),
 });
 
@@ -69,8 +89,8 @@ const AireAspiration = ({
 }) => {
   let { penaId } = useParams();
   penaId = peiIdCarte ?? penaId;
+  const { srid } = useAppContext();
 
-  // TODO get initial values
   const listeAireAspirationState = useGet(
     url`/api/pena/get-aire-aspiration/` + penaId,
   );
@@ -80,7 +100,7 @@ const AireAspiration = ({
   return (
     data && (
       <MyFormik
-        initialValues={getInitialValues(data)}
+        initialValues={getInitialValues(data, srid)}
         validationSchema={validationSchema}
         isPost={false}
         submitUrl={`/api/pena/upsert-pena-aspiration/` + penaId}
@@ -95,7 +115,9 @@ const AireAspiration = ({
 };
 
 const FormAireAspiration = ({ peiIdCarte }: { peiIdCarte?: string }) => {
-  const { values } = useFormikContext();
+  const { values } = useFormikContext<{
+    listeAireAspiration: AireAspirationType[];
+  }>();
   const { srid } = useAppContext();
 
   return (
@@ -122,6 +144,9 @@ const FormAireAspiration = ({ peiIdCarte }: { peiIdCarte?: string }) => {
               coordonneeX: null,
               coordonneeY: null,
               srid: srid,
+              typeSystemeSrid: TypeSystemeSrid[0].srid,
+              coordonneeXToDisplay: null,
+              coordonneeYToDisplay: null,
             }}
             listeElements={values.listeAireAspiration}
           />
@@ -140,7 +165,82 @@ const ComposantToRepeat = ({
   listeElements: AireAspirationType[];
 }) => {
   const typePenaAspirationState = useGet(url`/api/pena/type-pena-aspiration`);
-  const { setFieldValue } = useFormikContext();
+  const { setFieldValue } = useFormikContext<{
+    listeAireAspiration: AireAspirationType[];
+  }>();
+  const { srid } = useAppContext();
+
+  const element = listeElements[index];
+
+  const sridList = TypeSystemeSrid.filter(
+    (v) => v.actif || v.srid === Number(srid),
+  ).map((v) => ({
+    id: `${v.srid}`,
+    code: `${v.srid}`,
+    libelle: v.nomSystem,
+  }));
+
+  // Récupération des géométries selon le SRID
+  const geometrieState = useGet(
+    url`/api/pei/get-geometrie-by-srid?${{
+      coordonneeX: element.coordonneeXToDisplay,
+      coordonneeY: element.coordonneeYToDisplay,
+      srid: element.typeSystemeSrid,
+    }}`,
+  );
+
+  const geometrieData = geometrieState?.data;
+
+  // Conversion des coordonnées
+  useEffect(() => {
+    if (
+      element.coordonneeXToDisplay != null &&
+      element.coordonneeYToDisplay != null &&
+      element.typeSystemeSrid != null
+    ) {
+      geometrieState?.run({
+        coordonneeX: element.coordonneeXToDisplay,
+        coordonneeY: element.coordonneeYToDisplay,
+        srid: element.typeSystemeSrid,
+      });
+    }
+  }, [
+    element.coordonneeXToDisplay,
+    element.coordonneeYToDisplay,
+    element.typeSystemeSrid,
+    geometrieState,
+  ]);
+
+  // Mettre à jour les coordonnées converties
+  useEffect(() => {
+    if (
+      element.coordonneeXToDisplay !== undefined &&
+      element.coordonneeYToDisplay !== undefined
+    ) {
+      const coordonnees = geometrieData?.find(
+        (e: any) => Number(e.srid) === Number(srid),
+      );
+
+      if (coordonnees != null) {
+        setFieldValue(
+          `listeAireAspiration[${index}].coordonneeX`,
+          coordonnees?.coordonneeX,
+        );
+        setFieldValue(
+          `listeAireAspiration[${index}].coordonneeY`,
+          coordonnees?.coordonneeY,
+        );
+        setFieldValue(`listeAireAspiration[${index}].srid`, srid);
+      }
+    }
+  }, [
+    element.coordonneeXToDisplay,
+    element.coordonneeYToDisplay,
+    srid,
+    setFieldValue,
+    geometrieData,
+    index,
+  ]);
 
   return (
     <div>
@@ -158,7 +258,8 @@ const ComposantToRepeat = ({
             label="Dispositif d'aspiration"
             listIdCodeLibelle={typePenaAspirationState.data}
             defaultValue={typePenaAspirationState?.data?.find(
-              (e) => e.id === listeElements[index].typePenaAspirationId,
+              (e: { id: string }) =>
+                e.id === listeElements[index].typePenaAspirationId,
             )}
             required={false}
             setFieldValue={setFieldValue}
@@ -184,32 +285,86 @@ const ComposantToRepeat = ({
             label={"Est déportée"}
           />
         </Col>
-        {listeElements[index].estDeporte && (
-          <>
-            <Col>
-              <NumberInput
-                name={`listeAireAspiration[${index}].srid`}
-                label="SRID"
-                required={false}
-              />
-            </Col>
-            <Col>
-              <NumberInput
-                name={`listeAireAspiration[${index}].coordonneeX`}
-                label="Coordonnée X"
-                required={false}
-              />
-            </Col>
-            <Col>
-              <NumberInput
-                name={`listeAireAspiration[${index}].coordonneeY`}
-                label="Coordonnée Y"
-                required={false}
-              />
-            </Col>
-          </>
-        )}
       </Row>
+      {listeElements[index].estDeporte && (
+        <Row className="mt-3">
+          <Col xs={4}>
+            <SelectForm
+              label="Système"
+              name={`listeAireAspiration[${index}].typeSystemeSrid`}
+              required={true}
+              listIdCodeLibelle={sridList}
+              defaultValue={sridList.find(
+                (v) => v.id === `${element.typeSystemeSrid}`,
+              )}
+              onChange={(e: any) => {
+                setFieldValue(
+                  `listeAireAspiration[${index}].typeSystemeSrid`,
+                  parseInt(e.code),
+                );
+                const sridActif = e.code;
+                const projectionValeur = geometrieData?.find(
+                  (proj: any) => proj.srid === parseInt(sridActif),
+                );
+
+                if (projectionValeur) {
+                  setFieldValue(
+                    `listeAireAspiration[${index}].coordonneeXToDisplay`,
+                    projectionValeur.coordonneeX,
+                  );
+                  setFieldValue(
+                    `listeAireAspiration[${index}].coordonneeYToDisplay`,
+                    projectionValeur.coordonneeY,
+                  );
+
+                  // Coordonnées en SRID système
+                  const coordonneesToSave = geometrieData?.find(
+                    (proj: any) => proj.srid === srid,
+                  );
+                  if (coordonneesToSave) {
+                    setFieldValue(
+                      `listeAireAspiration[${index}].coordonneeX`,
+                      coordonneesToSave.coordonneeX,
+                    );
+                    setFieldValue(
+                      `listeAireAspiration[${index}].coordonneeY`,
+                      coordonneesToSave.coordonneeY,
+                    );
+                    setFieldValue(`listeAireAspiration[${index}].srid`, srid);
+                  }
+                }
+              }}
+              setFieldValue={setFieldValue}
+            />
+          </Col>
+          <Col xs={4}>
+            <TextInput
+              name={`listeAireAspiration[${index}].coordonneeXToDisplay`}
+              label="Coordonnée X"
+              required={listeElements[index].estDeporte}
+              onChange={(e) => {
+                setFieldValue(
+                  `listeAireAspiration[${index}].coordonneeXToDisplay`,
+                  e.target.value,
+                );
+              }}
+            />
+          </Col>
+          <Col xs={4}>
+            <TextInput
+              name={`listeAireAspiration[${index}].coordonneeYToDisplay`}
+              label="Coordonnée Y"
+              required={listeElements[index].estDeporte}
+              onChange={(e) => {
+                setFieldValue(
+                  `listeAireAspiration[${index}].coordonneeYToDisplay`,
+                  e.target.value,
+                );
+              }}
+            />
+          </Col>
+        </Row>
+      )}
     </div>
   );
 };
@@ -228,6 +383,9 @@ type AireAspirationType = {
   coordonneeX: string;
   coordonneeY: string;
   srid: number;
+  typeSystemeSrid: number;
+  coordonneeXToDisplay: string;
+  coordonneeYToDisplay: string;
 };
 
 export default AireAspiration;
