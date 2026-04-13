f92722c1e99c5a75be4c710c9dfd53d830ab6799
Emilie Genton <emg@atolcd.com>
2026-04-13 09:43:14 +0200

    Ajoute la version 3.3 comme version mobile compatible
    Change-Id: I33d8dcdb3ba316c1a50182c1aa6b0510d1218724

--- app/src/main/kotlin/remocra/apimobile/CompatibleVersions.kt
+++ app/src/main/kotlin/remocra/apimobile/CompatibleVersions.kt
@@ -8,7 +8,7 @@ package remocra.apimobile
 class CompatibleVersions {
     // Versions autorisées pour l'appli mobile
     private val mobileCompatibleVersions: MutableList<String?> =
-        mutableListOf(MobileVersion.M3_0.version, MobileVersion.M3_1.version, MobileVersion.M3_2.version)
+        mutableListOf(MobileVersion.M3_0.version, MobileVersion.M3_1.version, MobileVersion.M3_2.version, MobileVersion.M3_3.version)
 
     /**
      * Permet de savoir si la version de l'application mobile est compatible avec celle du serveur
@@ -29,5 +29,6 @@ class CompatibleVersions {
         M3_0("3.0"),
         M3_1("3.1"),
         M3_2("3.2"),
+        M3_3("3.3"),
     }
 }
