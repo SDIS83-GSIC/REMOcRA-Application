e2d684047e782b8691e14c47f49ace0e9787b641
mbe <mbe@atolcd.com>
2026-04-30 08:31:28 +0200

    Modifie le libellé pour le recalcul dans la mise a jour des positions des peis
    Issue: #289923
    Change-Id: I6aaadcd2b27314349ab93ab5ff0cc2a8dd490eaf

--- app/src/main/kotlin/remocra/data/enums/ErreurImportPei.kt
+++ app/src/main/kotlin/remocra/data/enums/ErreurImportPei.kt
@@ -9,7 +9,7 @@ enum class ErreurImportPei(val libelleCourt: String, val gravite: Gravite, val l
     ERR_FICHIER_INACCESSIBLE("Fichier inaccessible", Gravite.ERROR, "Impossible d'ouvrir le fichier"),
     ERR_EPSG_MANQUANT("EPSG manquant", Gravite.ERROR, "L'EPSG est manquant ou invalide."),
     ERR_EPSG_NORMALISATION("EPSG normalisation", Gravite.ERROR, "Erreur lors de la transformation de SRID."),
-    ERR_COMPUTE_NUMERO("Numéro à recalculer", Gravite.WARNING, "Le numéro du PEI sera recalculé"),
+    ERR_COMPUTE_NUMERO("Numéro à recalculer", Gravite.WARNING, "Le numéro du PEI sera recalculé si nécessaire"),
     ERR_DATE_MAL_FORMEE("Date mal formée", Gravite.WARNING, "La date du PEI est mal formée"),
     ERR_PEI_INEXISTANT("PEI inexistant", Gravite.ERROR, "Le PEI n'existe pas"),
     ERR_PEI_ZONE_COMPETENCE("Le PEI sort de la zone de compétence", Gravite.ERROR, "Le déplacement du PEI sort de la zone de compétence de l'utilisateur."),
